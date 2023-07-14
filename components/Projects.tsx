2eaff301b596b62a12cf7df496f0a094b70a68a5
Mkhuzo <zulumkhuzo@gmail.com>
2023-07-14 16:42:58 +0800

    fix weird hero issue

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -37,21 +37,3 @@ function Projects({ projects }: Props) {
 }
 
 export default Projects;
-
-{
-  /* <div className="mt-10">
-                    <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                        {features.map((feature) => (
-                        <div key={feature.name} className="relative">
-                            <dt>
-                            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                <feature.icon className="h-6 w-6" aria-hidden="true" />
-                            </div>
-                            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
-                            </dt>
-                            <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
-                        </div>
-                        ))}
-                    </dl>
-                    </div> */
-}
